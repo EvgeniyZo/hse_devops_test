ed81f05c50bca24f24d84676c3e78c3092364fe8
MateussssZ <mashimorin@edu.hse.ru>
2024-04-04 11:40:03 +0300

    divide 1 func to 5 funcs

--- kNN/test.py
+++ kNN/test.py
@@ -1,7 +1,7 @@
 from kNN import *
 import datetime
 
-def test_kNN():
+def test_kNN_2dots():
     knn = KNN(k=1)
     X_train =  np.array([[1, 1], [2, 2]])
     y_train =  np.array([0, 1])
@@ -9,6 +9,7 @@ def test_kNN():
     knn.fit(X_train, y_train)
     assert knn.predict(X_test) == [0]
 
+def test_kNN_MoreDots():
     knn = KNN(k=3)
     X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
     y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
@@ -16,20 +17,23 @@ def test_kNN():
     knn.fit(X_train, y_train)
     assert knn.predict(X_test) == [1]
 
+def test_kNN_test1k3():
     knn = KNN(k=3)
     X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
-    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
-    X_test = np.array([[5.5, 5.5]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0])
+    X_test = np.array([[7, 7]])
     knn.fit(X_train, y_train)
     assert knn.predict(X_test) == [1]
 
-    knn = KNN(k=3)
+def test_kNN_test1k7():
+    knn = KNN(k=7)
     X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
-    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
-    X_test = np.array([[15, 15]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0])
+    X_test = np.array([[7, 7]])
     knn.fit(X_train, y_train)
     assert knn.predict(X_test) == [1]
 
+def test_kNN_2TargetDots():
     knn = KNN(k=3)
     X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
     y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
