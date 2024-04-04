7300ff172d3c170b2bd44ca6433b5a91789e946f
MateussssZ <mashimorin@edu.hse.ru>
2024-04-04 11:46:06 +0300

    rewrite test

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,10 +1,46 @@
 from kNN.kNN import *
+import datetime
 
-
-def test_kNN():
+def test_kNN_2dots():
     knn = KNN(k=1)
-    X_train = np.array([[1, 1], [2, 2]])
-    y_train = np.array([0, 1])
-    X_test = np.array([[1.5, 1.5]])
+    X_train =  np.array([[1, 1], [2, 2]])
+    y_train =  np.array([0, 1])
+    X_test =  np.array([[1.5, 1.5]])
     knn.fit(X_train, y_train)
     assert knn.predict(X_test) == [0]
+
+def test_kNN_MoreDots():
+    knn = KNN(k=3)
+    X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    X_test = np.array([[9.5, 9.5]])
+    knn.fit(X_train, y_train)
+    assert knn.predict(X_test) == [1]
+
+def test_kNN_test1k3():
+    knn = KNN(k=3)
+    X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0])
+    X_test = np.array([[7, 7]])
+    knn.fit(X_train, y_train)
+    assert knn.predict(X_test) == [1]
+
+def test_kNN_test1k7():
+    knn = KNN(k=7)
+    X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0])
+    X_test = np.array([[7, 7]])
+    knn.fit(X_train, y_train)
+    assert knn.predict(X_test) == [1]
+
+def test_kNN_2TargetDots():
+    knn = KNN(k=3)
+    X_train = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    X_test = np.array([[5, 5], [2, 2]])
+    knn.fit(X_train, y_train)
+    assert all(knn.predict(X_test) == [1, 0])
+
+def test_current_time():
+    now = datetime.datetime.now()
+    assert now!=datetime.datetime.now()
