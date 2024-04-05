e5f7883764e7ea28c667745c1338387bcf7ca477
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-04-05 11:22:41 +0300

    make code more readable

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main(){
-	long long answ=1, n;
-	std::cin >> n;
-	for (int i=0;i<n;i++){
-		answ *= (i+1);
-	}
-	std::cout<<answ;
-	return 0;
+int main() {
+    long long answ = 1, n;
+    std::cin >> n;
+
+    for (long long i = 0; i < n; ++i) {
+        answ *= (i + 1);
+    }
+
+    std::cout << answ;
+    return 0;
 }
