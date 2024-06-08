04f9cca4bfce5f7ded270e781cbadc5302a64569
Aleksey <tryaskov.alex@yandex.ru>
2024-06-08 22:34:08 +0300

    first fixes

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int main(){
+int main(){ 
 	long long answ=1, n;
 	std::cin >> n;
 	for (int i=0;i<n;i++){
 		answ *= (i+1);
 	}
 	std::cout<<answ;
-	return 1;
+	return 1;// Очень важная правка
 }
